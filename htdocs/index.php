b72fcdc72fe32fb8466c71d729909d6567882f10
simplon <basilesimplon@gmail.com>
2021-03-15 16:48:04 +0100

    sauvegarde

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,10 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<?php include 'structure-page/header.php'; ?>
+<?php session_start();
+ include 'structure-page/head.php'; ?>
 <body>
+
+<?php if (!empty($_GET["message"])) : ?>
+        <div style="padding: 10px;background:gray;color:#fff;">
+            <?=$_GET["message"]?>
+        </div>
+<?php endif;?>
+
+<div class="like">
+<button><ion-icon name="caret-up-outline"></ion-icon></button>
+</div>
 
 </body>
 
 
 <?php include 'structure-page/footer.php'; ?>
+
 </html>
--- htdocs/structure-page/head.php
+++ htdocs/structure-page/head.php
@@ -1,5 +1,6 @@
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -19,8 +20,8 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">discussions</a>
+      <li class="nav-item">
+          <a class="nav-link" href="#">Discussions</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Deals</a>
@@ -33,20 +34,27 @@
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+            ...
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Toutes les catégories</a></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="#">TECH</a></li>
+            <li><a class="dropdown-item" href="#">HOME</a></li>
+            <li><a class="dropdown-item" href="#">COMICS & GRAPHI</a></li>
+            <li><a class="dropdown-item" href="#">WEB APP</a></li>
           </ul>
         </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
       </ul>
+      <div class="d-grid gap-2 d-md-flex justify-content-md-end">
 
+      <?php if (isset($_SESSION['nickname'])){ ?>
+        <a href="recuperation-donnees/deco_login.php"><button class="btn btn-secondary me-md-2" type="button">LOG OUT</button></a>
+      <?php }else{ ?>
+        <a href="structure-page/login.php"><button class="btn btn-secondary me-md-2" type="button">LOG IN</button></a>
+        <a href="structure-page/signup.php"><button class="btn btn-danger" type="button" >SIGN UP</button></a>
+      <?php } ?>
+      </div>
     </div>
   </div>
 </nav>
